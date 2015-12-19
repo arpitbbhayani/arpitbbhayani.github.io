28e17a4911f86c50557a68de3fd0651197699f6b
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-12-19 20:11:05 +0530

    publish script v1

---
+++ scripts/publish.sh
@@ -0,0 +1,18 @@
+# Copies _site folder
+
+ROOT=/Users/arpitbhayani/arpitbbhayani.github.io/
+TMP_FOLDER=/tmp/arpitbbhayani.github.io
+SITE_DIR=/Users/arpitbhayani/arpitbbhayani.github.io/_site/
+
+if [ -d $TMP_FOLDER ]; then
+    rm -rf $TMP_FOLDER/*
+else;
+    mkdir $TMP_FOLDER
+fi
+
+cp $SITE_DIR/* $TMP_FOLDER
+
+cd $ROOT
+git checkout master
+cp $TMP_FOLDER/* .
+git status
