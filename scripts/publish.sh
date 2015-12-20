12ff834177afa002146bd014d0fe359c801f7b33
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-12-20 16:46:55 +0530

    publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,29 @@
+# Copies _site folder
+
+ROOT=/Users/arpitbhayani/arpitbbhayani.github.io/
+TMP_FOLDER=/tmp/arpitbbhayani.github.io
+SITE_DIR=/Users/arpitbhayani/arpitbbhayani.github.io/_site/
+
+if [ -d $TMP_FOLDER ]; then
+    rm -rf $TMP_FOLDER/*
+else
+    mkdir $TMP_FOLDER
+fi
+
+cp -r $SITE_DIR/* $TMP_FOLDER
+
+cd $ROOT
+git checkout master
+cd $ROOT
+
+cp -r $TMP_FOLDER/* .
+git status
+
+read -p "Do you wish to continue with publishing it to master?" yn
+case $yn in
+    [Yy]* ) git add --all; git commit -m 'Publishing latest site'; git push origin master; break;;
+    [Nn]* ) git reset --hard origin/master; exit;;
+    * ) echo "Please answer yes or no.";;
+esac
+
+git checkout source
