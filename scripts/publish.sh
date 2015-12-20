ebe226a93a723f35b8e06374d4e399c6646bd3da
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-12-20 16:54:23 +0530

    Cleaning code

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,23 +1,20 @@
 # Copies _site folder
 
 ROOT=/Users/arpitbhayani/arpitbbhayani.github.io/
-TMP_FOLDER=/tmp/arpitbbhayani.github.io
+TMP_FOLDER=/tmp/arpitbbhayani.github.io/
 SITE_DIR=/Users/arpitbhayani/arpitbbhayani.github.io/_site/
 
 if [ -d $TMP_FOLDER ]; then
-    rm -rf $TMP_FOLDER/*
-else
-    mkdir $TMP_FOLDER
+    rm -rf $TMP_FOLDER
 fi
+mkdir $TMP_FOLDER
+
+cd $ROOT
 
 jekyll build
-
 cp -r $SITE_DIR/* $TMP_FOLDER
 
-cd $ROOT
 git checkout master
-cd $ROOT
-
 cp -r $TMP_FOLDER/* .
 git status
 
@@ -29,3 +26,6 @@ case $yn in
 esac
 
 git checkout source
+
+echo 'Cleaning ...'
+echo "Removing $TMP_FOLDER" && rm -rf $TMP_FOLDER
