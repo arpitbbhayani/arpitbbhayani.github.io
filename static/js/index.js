280638a3dc283340fab5b8dd4930045e4eeb0bce
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-12-26 14:13:02 +0530

    Publishing latest site

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $('.menu .item').tab();
     $('.ui.dropdown').dropdown();
 
+    $('[data-content]').popup();
+
     $('.ui.label').click(function() {
         $(this).transition('pulse');
     });
