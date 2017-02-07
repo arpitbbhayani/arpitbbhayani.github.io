1f1cb205c2ffffe8276bb39854af8d27d443aa26
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2017-02-08 01:44:51 +0530

    Publishing latest site

--- static/js/index.js
+++ static/js/index.js
@@ -7,23 +7,9 @@ $(document).ready(function() {
         $(this).transition('pulse');
     });
 
-    var amountScrolled = 100;
-    var $main_nav_page_title_obj = $('.main-nav-page-title');
-
-    $main_nav_page_title_obj.hide();
-
-    $(window).scroll(function() {
-    	if ( $(window).scrollTop() > amountScrolled ) {
-            $main_nav_page_title_obj.fadeIn('slow');
-    	} else {
-            $main_nav_page_title_obj.fadeOut('slow');
-    	}
-    });
-
     scrollProgress.set({
         color: '#f87c7c',
         height: '2px',
         bottom: false
     });
-
 });
