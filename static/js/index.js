5cc51784b5dd1a4986a225f8f9cecd3496b23692
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2017-05-03 23:30:03 +0530

    Publishing latest site

--- static/js/index.js
+++ static/js/index.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
     $('.menu .item').tab();
     $('.ui.dropdown').dropdown();
     $('[data-content]').popup();
+
+    $('#menu_sidebar').sidebar({
+      dimPage: false,
+      transition: 'overlay',
+      mobileTransition: 'overlay'
+    });
+
+    $('#menu_button, #close_button').click(function(e) {
+      e.preventDefault();
+      $('#menu_sidebar').sidebar('toggle');
+    });
 });
